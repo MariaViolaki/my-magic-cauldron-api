92793a7ebb693fe33d5c5cb0db52196f8e237111
MariaViolaki <mariaviolaki@outlook.com>
2019-02-17 19:10:41 +0200

    Fix login functionality

--- controllers/login.js
+++ controllers/login.js
@@ -1,5 +1,6 @@
 const handleLogIn = (req, res, db, bcrypt) => {
 	const { username_email, password } = req.body;
+	const profile = [];
 	
 	if (username_email.length > 0 && password.length > 0) {
 
@@ -17,7 +18,20 @@ const handleLogIn = (req, res, db, bcrypt) => {
 				.where('username', '=', username_email)
 				.orWhere('email', '=', username_email)
 				.then(user => {
-					res.json(user[0]);
+					profile.push(user[0]);
+				})
+				.then(() => {
+					return db('potions')
+					.select('*')
+					.where('username', '=', profile[0].username)
+					.then(potions => {
+						profile.push(potions[0]);
+						console.log(profile);
+						res.json(profile);
+					})
+					.catch(err => {
+						res.status(400).json('Unable to get potions');
+					});
 				})
 				.catch(err => {
 					res.status(400).json('Unable to get user');
